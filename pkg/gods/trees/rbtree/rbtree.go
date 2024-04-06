bc032b30e92eec5eeb066f9047ef6845c0528ade
Sean Zheng <blackhorseya@gmail.com>
2024-04-06 18:58:35 +0800

    chore: consolidate RBTree implementation improvements
    - Remove the todo comment from rbtree.go
    - Define constants ColorRed and ColorBlack in rbtree.go
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/gods/trees/rbtree/rbtree.go
+++ pkg/gods/trees/rbtree/rbtree.go
@@ -1,3 +1,21 @@
 package rbtree
 
-// todo: 2024/4/4|sean|implement me
+// Item is the interface that the tree will use to store items.
+type Item interface {
+	Less(than Item) bool
+}
+
+const (
+	ColorRed   = false
+	ColorBlack = true
+)
+
+// Node is a single element within the tree.
+type Node struct {
+	Item
+
+	Left   *Node
+	Right  *Node
+	Parent *Node
+	Color  bool
+}
