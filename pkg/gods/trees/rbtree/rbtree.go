73b2ca74db1564e8d9837947e633e93a775e7960
Sean Zheng <blackhorseya@gmail.com>
2024-04-06 19:03:37 +0800

    feat: initialize RBTree with NIL node
    - Add initialization of `NIL` node in `RBTree`
    - Initialize `root` node in `RBTree` with `NIL` node
    - Implement `New` function to return an initialized red-black tree
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/gods/trees/rbtree/rbtree.go
+++ pkg/gods/trees/rbtree/rbtree.go
@@ -19,3 +19,32 @@ type Node struct {
 	Parent *Node
 	Color  bool
 }
+
+// RBTree is a red-black tree.
+type RBTree struct {
+	NIL *Node
+
+	root  *Node
+	count uint
+}
+
+// New returns an initialized red-black tree.
+func New() *RBTree {
+	node := &Node{
+		Item:   nil,
+		Left:   nil,
+		Right:  nil,
+		Parent: nil,
+		Color:  true,
+	}
+
+	return &RBTree{
+		NIL:   node,
+		root:  node,
+		count: 0,
+	}
+}
+
+func less(a, b Item) bool {
+	return a.Less(b)
+}
