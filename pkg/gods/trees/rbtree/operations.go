fe0083c58220a0546d122fbbf28ee9c74c7c0f02
Sean Zheng <blackhorseya@gmail.com>
2024-04-06 19:07:43 +0800

    feat: improve RBTree functionality with new operations
    - Add `operations.go` to the `srcs` list in `rbtree` go_library
    - Add `operations.go` file with basic functions for `RBTree` operations
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ pkg/gods/trees/rbtree/operations.go
@@ -0,0 +1,18 @@
+package rbtree
+
+// Len returns the number of items in the tree.
+func (t *RBTree) Len() uint {
+	return t.count
+}
+
+// Insert adds an item to the tree.
+func (t *RBTree) Insert(item Item) {
+	// todo: 2024/4/6|sean|implement me
+	panic("implement me")
+}
+
+// Delete removes an item from the tree.
+func (t *RBTree) Delete(item Item) Item {
+	// todo: 2024/4/6|sean|implement me
+	panic("implement me")
+}
