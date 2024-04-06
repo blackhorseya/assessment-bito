abf971e64d022512abf5c9974ca11a919b062337
Sean Zheng <blackhorseya@gmail.com>
2024-04-06 19:58:05 +0800

    chore: refactor rbtree implementation in player repository
    - Add a new dependency on `pkg/gods/trees/rbtree` in `BUILD.bazel`
    - Rename `rbtree` to `rbtreeImpl` and update method signatures in `rbtree.go`
    - Initialize `rbtree` in `NewPlayerRepoWithRBTree` method in `rbtree.go`
    - Change receiver type from `rbtree` to `rbtreeImpl` in methods in `rbtree.go`
    - Update method signatures in `rbtree.go` to use `rbtreeImpl` instead of `rbtree`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/match/repo/player/rbtree.go
+++ app/domain/match/repo/player/rbtree.go
@@ -7,33 +7,37 @@ import (
 	"github.com/blackhorseya/assessment-bito/entity/domain/match/model"
 	"github.com/blackhorseya/assessment-bito/entity/domain/match/repo"
 	"github.com/blackhorseya/assessment-bito/pkg/contextx"
+	"github.com/blackhorseya/assessment-bito/pkg/gods/trees/rbtree"
 )
 
-type rbtree struct {
+type rbtreeImpl struct {
 	sync.RWMutex
+	rbtree *rbtree.RBTree
 }
 
-// NewPlayerRepoWithRBTree is to create a new player repo with rbtree.
+// NewPlayerRepoWithRBTree is to create a new player repo with rbtreeImpl.
 func NewPlayerRepoWithRBTree() repo.IPlayerRepo {
-	return &rbtree{}
+	return &rbtreeImpl{
+		rbtree: rbtree.New(),
+	}
 }
 
-func (i *rbtree) GetByID(ctx contextx.Contextx, id string) (item *agg.Player, err error) {
+func (i *rbtreeImpl) GetByID(ctx contextx.Contextx, id string) (item *agg.Player, err error) {
 	// todo: 2024/4/5|sean|implement me
 	panic("implement me")
 }
 
-func (i *rbtree) JoinPlayer(ctx contextx.Contextx, player *agg.Player) (err error) {
+func (i *rbtreeImpl) JoinPlayer(ctx contextx.Contextx, player *agg.Player) (err error) {
 	// todo: 2024/4/5|sean|implement me
 	panic("implement me")
 }
 
-func (i *rbtree) LeavePlayer(ctx contextx.Contextx, player *agg.Player) (err error) {
+func (i *rbtreeImpl) LeavePlayer(ctx contextx.Contextx, player *agg.Player) (err error) {
 	// todo: 2024/4/5|sean|implement me
 	panic("implement me")
 }
 
-func (i *rbtree) ListPlayers(
+func (i *rbtreeImpl) ListPlayers(
 	ctx contextx.Contextx,
 	condition repo.ListPlayersCondition,
 ) (items []*agg.Player, total int, err error) {
@@ -41,7 +45,7 @@ func (i *rbtree) ListPlayers(
 	panic("implement me")
 }
 
-func (i *rbtree) MatchedPair(ctx contextx.Contextx, left, right *agg.Player, pair *model.Pair) (err error) {
+func (i *rbtreeImpl) MatchedPair(ctx contextx.Contextx, left, right *agg.Player, pair *model.Pair) (err error) {
 	// todo: 2024/4/5|sean|implement me
 	panic("implement me")
 }
