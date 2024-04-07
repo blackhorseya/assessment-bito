0c89315b3adc7b3e3f2e2d6676436d6422128a85
Sean Zheng <blackhorseya@gmail.com>
2024-04-07 12:19:00 +0800

    feat: improve RBTree functionality and testing suite
    - Add a new `Get` method to retrieve an item from the RBTree
    - Include a new `testVar` struct for testing purposes
    - Add a new test for the `Get` method in the RBTree
    - Update the RBTree test to include insertion and deletion tests
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/gods/trees/rbtree/operations.go
+++ pkg/gods/trees/rbtree/operations.go
@@ -5,6 +5,21 @@ func (t *RBTree) Len() uint {
 	return t.count
 }
 
+// Get returns the item in the tree that is equal to the given item.
+func (t *RBTree) Get(item Item) Item {
+	if item == nil {
+		return nil
+	}
+
+	// The `color` field here is nobody
+	ret := t.search(&Node{t.NIL, t.NIL, t.NIL, RED, item})
+	if ret == nil {
+		return nil
+	}
+
+	return ret.Item
+}
+
 // Insert adds an item to the tree.
 func (t *RBTree) Insert(item Item) {
 	if item == nil {
--- pkg/gods/trees/rbtree/rbtree_test.go
+++ pkg/gods/trees/rbtree/rbtree_test.go
@@ -25,3 +25,44 @@ func TestInsertAndDelete(t *testing.T) {
 		t.Errorf("tree.Len() = %d, expect %d", rbt.Len(), 1)
 	}
 }
+
+type testVar struct {
+	Height uint
+	ID     string
+}
+
+func (t *testVar) Less(than Item) bool {
+	return t.Height < than.(*testVar).Height
+}
+
+func TestGet(t *testing.T) {
+	rbt := New()
+
+	rbt.Insert(&testVar{
+		Height: 170,
+		ID:     "1",
+	})
+
+	got := rbt.Get(&testVar{
+		Height: 170,
+		ID:     "1",
+	})
+
+	if got == nil {
+		t.Errorf("tree.Get() = nil, expect not nil")
+	}
+
+	rbt.Delete(&testVar{
+		Height: 170,
+		ID:     "1",
+	})
+
+	got = rbt.Get(&testVar{
+		Height: 170,
+		ID:     "1",
+	})
+
+	if got != nil {
+		t.Errorf("tree.Get() = %v, expect nil", got)
+	}
+}
